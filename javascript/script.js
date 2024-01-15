e42b95ac48a8efd990512dedd6e1861a0d23a2cd
DimsRK <dimasrestukumbayohni@gmail.com>
2024-01-15 23:21:21 +0700

    fungsi perhitungan luas & keliling,, baru diterapkan ke console

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function keliling() {
+    let sisiPersegi= document.getElementById("perimeterInput").value;
+    let KelilingPersegi= parseInt(sisiPersegi) * 4
+    console.log(KelilingPersegi)
+}
+
+function luas() {
+    let sisiPersegi= document.getElementById("areaInput").value;
+    let luasPersegi= parseInt(sisiPersegi) * parseInt(sisiPersegi)
+    console.log(luasPersegi)
+}
