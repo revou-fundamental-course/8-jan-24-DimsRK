08d200b620d4ac255479ace48fc90a4ba1f761b1
DimsRK <dimasrestukumbayohni@gmail.com>
2024-01-18 00:05:59 +0700

    pr nya tinggal nambahin fungsi biar input harus number, sama result yg luas kok gabisa direset

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,44 @@ function keliling() {
     let sisiPersegi= document.getElementById("perimeterInput").value;
     let kelilingPersegi= parseFloat(sisiPersegi) * 4
     console.log(kelilingPersegi)
-    document.getElementById("resultPer").innerHTML = "<p>Keliling Persegi Adalah</p>" + kelilingPersegi;
+    document.getElementById("perResult").innerHTML = /*"<p>Keliling Persegi Adalah :</p>"*/ + kelilingPersegi;
 }
 
 function luas() {
     let sisiPersegi= document.getElementById("areaInput").value;
     let luasPersegi= parseFloat(sisiPersegi) * parseFloat(sisiPersegi)
     console.log(luasPersegi)
-    document.getElementById("resultAre").innerHTML = "<p>Luas Persegi Adalah</p>" + luasPersegi;
+    document.getElementById("resultAre").innerHTML = "<p>Luas Persegi Adalah :</p>" + luasPersegi;
 }
+
+function reset1() {
+    var resultPer = document.getElementById("perResult");
+    var perimeterSide = document.getElementById("perimeterInput");
+  
+    if (resultPer) {
+      resultPer.innerHTML = "";
+    }
+  
+    if (perimeterSide) {
+      perimeterSide.value = "";
+    }
+} 
+
+function reset2() {
+    var resultAre = document.getElementById("areResult");
+    var areaSide = document.getElementById("areaInput");
+  
+    if (resultAre) {
+      resultAre.innerHTML = "";
+    }
+  
+    if (areaSide) {
+      areaSide.value = "";
+    }
+} 
+  
+/*function reset() {
+    document.getElementsByClassName("perimeterInput").value = "";
+    document.getElementById("result").innerHTML = "";
+}  
+*/
