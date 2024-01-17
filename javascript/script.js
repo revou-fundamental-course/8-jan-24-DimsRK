15244f8179c1b4c579e2d565cab68eeb40a91cb3
DimsRK <dimasrestukumbayohni@gmail.com>
2024-01-17 21:56:21 +0700

    almost cari solusi untuk tampilan hasil

--- javascript/script.js
+++ javascript/script.js
@@ -2,11 +2,12 @@ function keliling() {
     let sisiPersegi= document.getElementById("perimeterInput").value;
     let kelilingPersegi= parseFloat(sisiPersegi) * 4
     console.log(kelilingPersegi)
-    document.getElementById("resultPer").innerHTML = "<p></p>" + kelilingPersegi;
+    document.getElementById("resultPer").innerHTML = "<p>Keliling Persegi Adalah</p>" + kelilingPersegi;
 }
 
 function luas() {
     let sisiPersegi= document.getElementById("areaInput").value;
     let luasPersegi= parseFloat(sisiPersegi) * parseFloat(sisiPersegi)
     console.log(luasPersegi)
+    document.getElementById("resultAre").innerHTML = "<p>Luas Persegi Adalah</p>" + luasPersegi;
 }
